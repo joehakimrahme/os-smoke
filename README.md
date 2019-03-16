a5b758d7637376edb272b7ddc0bd6ceeb8ffd1f6
Joe H. Rahme <jhakimra@redhat.com>
2019-03-16 17:39:10 +0100

    Rework and rename
    * Called os-smoke now
    * 2 functions: init and cleanup
    * Drop the cli parsing in favor of a local.conf approach

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-RHOS Smoke
-==========
+os-smoke.sh
+===========
 
 A quick utility to set up networking and launch a single instance on a
-fresh Infrared deployment. I use this as a quick sanity test, which I
-find faster than setting up Tempest.
+fresh OpenStack deployment. I use this as a quick sanity test, which I
+find much faster than setting up Tempest.
 
 Usage
 -----
-	
-	$ smoke.sh [-i image] [-p provider-network]
-    Populates the overcloud with an image and an instance. Boot an instance. Assign a floating IP.
-    
-    Arguments:
-      -h, --help: Print this help message and exits.
-      -i, --image: Defines the image used in the guest (default: cirros)
-      -p, --provider-network: Defines the name of the provider network (default: public)
-      
+
+	$ ./os-smoke.sh
+	Usage: ./os-smoke.sh (init|cleanup)
+
+`os-smoke.sh` is a script that takes an action argument:
+
+* `./os-smoke.sh init` will create resources and spawn an instance
+* `./os-smoke.sh cleanup` will destroy the instance and free the resources
+
+
+Configuration
+-------------
+
+The behavior of the script can be configured by defining environment
+variables, either in the shell or by defining them in a `local.conf`
+file.
+
+* `SMK_RCFILE`: a path to the cloud's rc file
+* `SMK_IMG_NAME`: the name of the image to use on the instance
+* `SMK_IMG_URL`: (optional) path to a qcow2 image to upload to the
+  cloud, in case the image `SMK_IMG_NAME` isn't available.
+
+A `local.conf.sample` has been provided with this script and can serve
+as a starting point for configuration. The file comments go more in
+details in the description of each variable, and show each variable
+default value.
--- smoke.sh
+++ smoke.sh
@@ -9,87 +9,115 @@
 #
 # Tested on: RHOS13, RHOS14
 
-set -x -o pipefail
+# Fail early and print usage if no arg is supplied
+if [ -z $1 ]; then
+    echo "Usage: $0 (init|cleanup)"
+    exit
+fi
 
-OPTS=$(getopt -o hi:p: --long "help,image:,provider-network:" -n "$0" -- "$@")
-eval set -- "$OPTS"
-source /home/stack/overcloudrc.v3
-function usage {
-    echo "Usage: $0 [-i image] [-p provider-network]
-Populates the overcloud with an image and an instance. Boot an instance. Assign a floating IP.
+# Print the commands being run so that we can see the command that triggers
+# an error.  It is also useful for following along as the install occurs.
+set -o xtrace
 
-Arguments:
-  -h, --help: Print this help message and exits.
-  -i, --image: Defines the image used in the guest (default: cirros)
-  -p, --provider-network: Defines the name of the provider network (default: public)"
+# Some sanity safeguards
+unset GREP_OPTIONS
+umask 022
+
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+
+# source options from local.conf if it exists and set default values
+# if any is missing.
+if [ -f $TOP_DIR/local.conf ]; then
+    source $TOP_DIR/local.conf
+fi
+
+SMK_RCFILE=${SMK_RCFILE:-openrc}
+SMK_IMG_NAME=${SMK_IMG_NAME:-cirros}
+SMK_PROVIDER_NETWORK=${SMK_PROVIDER_NETWORK:-public}
+SMK_PRIVATE_NETWORK=${SMK_PRIVATE_NETWORK:-private}
+SMK_DEVSTACK_NETWORK=${SMK_DEVSTACK_NETWORK:-devstack}
+SMK_PRIVATE_SUBNET=${SMK_PRIVATE_SUBNET:-private-subnet}
+SMK_DEVSTACK_SUBNET=${SMK_DEVSTACK_SUBNET:-devstack-subnet}
+SMK_ROUTER_NAME=${SMK_ROUTER_NAME:-testrouter}
+SMK_INSTANCE_NAME=${SMK_INSTANCE_NAME:-devstack}
+
+
+if [ ! -f "$SMK_RCFILE" ]; then
+    die $LINENO "missing RC file. Set the variable SMK_RCFILE in the environment"
+fi
+source "$SMK_RCFILE"
+
+
+function init {
+
+    # Check that the provider network exists.
+    if ! openstack network list -c Name -f value | grep -q "$SMK_PROVIDER_NETWORK"; then
+	echo "Provider network $SMK_PROVIDER_NETWORK does not exist."
+	exit 2
+    fi
+
+    if ! openstack network list | grep -q "$SMK_PRIVATE_NETWORK"; then
+	openstack network create "$SMK_PRIVATE_NETWORK"
+    fi
+
+    if ! openstack network list | grep -q "$SMK_DEVSTACK_NETWORK"; then
+	openstack network create "$SMK_DEVSTACK_NETWORK"
+    fi
+
+    if ! openstack subnet list | grep -q "$SMK_DEVSTACK_SUBNET"; then
+	openstack subnet create --gateway 192.168.100.1 --dhcp --network "$SMK_DEVSTACK_NETWORK" --subnet-range 192.168.100.0/24 "$SMK_DEVSTACK_SUBNET"
+	openstack subnet set --dns-nameserver 10.34.32.1 --dns-nameserver 10.34.32.3 "$SMK_DEVSTACK_SUBNET"
+    fi
+
+    if ! openstack subnet list | grep -q "$SMK_PRIVATE_SUBNET"; then
+	openstack subnet create --gateway 192.168.200.1 --dhcp --network "$SMK_PRIVATE_NETWORK" --subnet-range 192.168.200.0/24 "$SMK_PRIVATE_SUBNET"
+	openstack subnet set --dns-nameserver 10.34.32.1 --dns-nameserver 10.34.32.3 "$SMK_PRIVATE_SUBNET"
+    fi
+
+    if ! openstack router list | grep "$SMK_ROUTER_NAME"; then
+	openstack router create "$SMK_ROUTER_NAME"
+	openstack router set --external-gateway "$SMK_PROVIDER_NETWORK" "$SMK_ROUTER_NAME"
+	openstack router add subnet "$SMK_ROUTER_NAME" "$SMK_DEVSTACK_SUBNET"
+	openstack router add subnet "$SMK_ROUTER_NAME" "$SMK_PRIVATE_SUBNET"
+    fi
+
+    # SECID=$(openstack security group list | grep $(openstack project show admin -f value -c id) | head -n 2 | awk '{print $2}')
+    # openstack security group rule create $SECID --protocol tcp --dst-port 22 --remote-ip 0.0.0.0/0 2>/dev/null
+    # openstack security group rule create $SECID --protocol icmp --dst-port -1 --remote-ip 0.0.0.0/0 2>/dev/null
+
+    # devstack_network_id=$(openstack network list -c ID -c Name -f value | awk '/devstack/ {print $1}')
+    # private_network_id=$(openstack network list -c ID -c Name -f value | awk '/private/ {print $1}')
 }
 
-while true; do
-    case "$1" in
-        -h | --help ) usage; exit 0;;
-        -i | --image ) image="$2"; shift 2;;
-        -p | --provider-network) provider_network="$2"; shift 2;;
-        * ) break;;
-    esac
-done
-RELEASE=$(</etc/yum.repos.d/latest-installed awk '{print $1}')
+function cleanup {
 
-# Redirect output to smoke.log
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>smoke.log 2>&1
-
-# Right now, only cirros is supported.
-if [ -z $(echo "$image" | grep -Ew 'cirros') ]; then
-    echo "Wrong image value. Supported value(s): 'cirros'"
-    usage
-    exit 1
-fi
-
-# Check that the provider network exists.
-if [ -z $(openstack network list | grep "$provider_network") ]; then
-    echo "Provider network $provider_network does not exist."
-    usage
-    exit 2
-fi
-
-
-if [ -z "$(openstack network list | grep private)" ]; then
-    openstack network create private
-    openstack subnet create --gateway 192.168.100.1 --dhcp --network private --subnet-range 192.168.100.0/24 private
-    openstack subnet set --dns-nameserver 10.34.32.1 --dns-nameserver 10.34.32.3 private
-    echo "****************************************Private network created****************************************************"
-fi
-
-SID=$(openstack network list -c ID -c Name -f value | awk '/private/ {print $1}')
-if [ -z "$(openstack router list | grep testrouter)" ];then
-    openstack router create testrouter
-    openstack router set --external-gateway "$provider_network" testrouter
-    openstack router add subnet testrouter private
-    echo "****************************************Router and subnet created*************************************************"
-fi
-
-SECID=$(openstack security group list | grep $(openstack project show admin -f value -c id) | head -n 2 | awk '{print $2}')
-openstack security group rule create $SECID --protocol tcp --dst-port 22 --remote-ip 0.0.0.0/0 2>/dev/null
-openstack security group rule create $SECID --protocol icmp --dst-port -1 --remote-ip 0.0.0.0/0 2>/dev/null
-
-if [ -z "$(openstack image list | grep cirros)" ];then
-    if [ ! -f cirros-0.3.5-x86_64-disk.img ]; then
-        wget http://rhos-qe-mirror-tlv.usersys.redhat.com/images/cirros-0.3.5-x86_64-disk.img
+    if [[ -n $(openstack server list -c Name -f value) ]]; then
+	openstack server delete "$SMK_INSTANCE_NAME"
     fi
-    IMGNAME=cirros
-    openstack image create $IMGNAME --disk-format qcow2 --container-format bare --file cirros-0.3.5-x86_64-disk.img
-    echo "****************************************Image uploaded to glance**************************************************"
-else
-    IMGNAME=$(openstack image list -f value -c Name | grep cirros | head -n 1)
-fi
 
-if [ -z "$(openstack flavor list | grep m1.smoke)" ]; then
-    openstack flavor create --public m1.smoke --id auto --ram 512 --disk 1 --vcpus 1
-fi
+    if [[ -n $(openstack port list -c Name -f value) ]]; then
+	if openstack router list -c Name -f value | grep -q "$SMK_ROUTER_NAME"; then
+	    openstack router remove subnet "$SMK_ROUTER_NAME" "$SMK_DEVSTACK_SUBNET"
+	    openstack router remove subnet "$SMK_ROUTER_NAME" "$SMK_PRIVATE_SUBNET"
+	    openstack router delete "$SMK_ROUTER_NAME"
+	fi
+    fi
 
-COUNTVAR=$RANDOM
-openstack server create --image $IMGNAME --flavor m1.smoke test-$COUNTVAR --nic net-id=$SID --wait
-IP=$(openstack floating ip create "$provider_network" -f value -c floating_ip_address)
-openstack server add floating ip test-$COUNTVAR $IP
-openstack server list
+    openstack subnet delete "$SMK_PRIVATE_SUBNET"
+    openstack subnet delete "$SMK_DEVSTACK_SUBNET"
+
+    openstack network delete "$SMK_PRIVATE_NETWORK"
+    openstack network delete "$SMK_DEVSTACK_NETWORK"
+
+}
+
+SMK_START_TIME=$(date +%s)
+if [ "$1" = "init" ]; then
+    init
+elif [ "$1" = "cleanup" ]; then
+    cleanup
+fi
+SMK_END_TIME=$(date +%s)
+
+set +o xtrace
+echo "Total execution time: $(( SMK_END_TIME - SMK_START_TIME )) seconds."
